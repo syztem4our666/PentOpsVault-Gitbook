bb5eaa6705fd0a4762f1634d930bd1465947a9c5
syztem4our666 <SYSTEMFOUR666@proton.me>
2025-02-05 23:23:00 +0100

    Added Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { RootProvider } from "fumadocs-ui/provider";
 import { Inter } from "next/font/google";
 import type { ReactNode } from "react";
 import { Banner } from 'fumadocs-ui/components/banner';
+import { Analytics } from '@vercel/analytics/next';
 
 const inter = Inter({
   subsets: ["latin"],
@@ -14,6 +15,8 @@ export default function Layout({ children }: { children: ReactNode }) {
       <body className="flex flex-col min-h-screen">
       <Banner variant="rainbow">PentOpsVault @syztem4our666</Banner>
         <RootProvider>{children}</RootProvider>
+        <Analytics />
+
       </body>
     </html>
   );
