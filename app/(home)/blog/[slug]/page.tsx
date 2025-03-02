589c4ea88092a538d8d5accbcee1496e5661ddc0
syztem4our666 <SYSTEMFOUR666@proton.me>
2025-03-02 18:16:11 +0100

    Fixed ESlint

--- app/(home)/blog/[slug]/page.tsx
+++ app/(home)/blog/[slug]/page.tsx
@@ -10,8 +10,9 @@ import { Control } from "@/app/(home)/blog/[slug]/page.client"
 import { File, Files, Folder } from "fumadocs-ui/components/files"
 import { Tab, Tabs } from "fumadocs-ui/components/tabs"
 
-export default async function Page({ params }: { params: { slug: string } }) {
-  const page = blog.getPage([params.slug])
+export default async function Page({ params }: { params: Promise<{ slug: string }> }) {
+  const resolvedParams = await params
+  const page = blog.getPage([resolvedParams.slug])
 
   if (!page) notFound()
 
@@ -20,24 +21,24 @@ export default async function Page({ params }: { params: { slug: string } }) {
   return (
     <>
       <div
-  className="container rounded-xl border py-12 md:px-8"
-  style={{
-    marginTop: "3rem", // Agrega un margen superior de 3rem
-    backgroundColor: "black",
-    backgroundImage: [
-      "linear-gradient(140deg, hsla(274,94%,54%,0.3), transparent 50%)",
-      "linear-gradient(to left top, hsla(260,90%,50%,0.8), transparent 50%)",
-      "radial-gradient(circle at 100% 100%, hsla(240,100%,82%,1), hsla(240,40%,40%,1) 17%, hsla(240,40%,40%,0.5) 20%, transparent)",
-    ].join(", "),
-    backgroundBlendMode: "difference, difference, normal",
-  }}
->
-  <h1 className="mb-2 text-3xl font-bold text-white">{page.data.title}</h1>
-  <p className="mb-4 text-white/80">{page.data.description}</p>
-  <Link href="/blog" className={buttonVariants({ size: "sm", variant: "secondary" })}>
-    Back
-  </Link>
-</div>
+        className="container rounded-xl border py-12 md:px-8"
+        style={{
+          marginTop: "3rem",
+          backgroundColor: "black",
+          backgroundImage: [
+            "linear-gradient(140deg, hsla(274,94%,54%,0.3), transparent 50%)",
+            "linear-gradient(to left top, hsla(260,90%,50%,0.8), transparent 50%)",
+            "radial-gradient(circle at 100% 100%, hsla(240,100%,82%,1), hsla(240,40%,40%,1) 17%, hsla(240,40%,40%,0.5) 20%, transparent)",
+          ].join(", "),
+          backgroundBlendMode: "difference, difference, normal",
+        }}
+      >
+        <h1 className="mb-2 text-3xl font-bold text-white">{page.data.title}</h1>
+        <p className="mb-4 text-white/80">{page.data.description}</p>
+        <Link href="/blog" className={buttonVariants({ size: "sm", variant: "secondary" })}>
+          Back
+        </Link>
+      </div>
 
       <article className="container flex flex-col px-0 py-8 lg:flex-row lg:px-4">
         <div className="prose min-w-0 flex-1 p-4">
